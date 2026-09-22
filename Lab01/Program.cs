b8e791ac35f1048d332835760d623372fd665590
Андрій Угрин <uhryn.andrii@chnu.edu.ua>
2026-09-23 02:18:27 +0300

    Lab01 Task04

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -5,4 +5,4 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 System.Threading.Thread.CurrentThread.CurrentCulture =
     System.Globalization.CultureInfo.InvariantCulture;
 
-Task3.Run();
+Task4.Run();
---
+++ Lab01/Task4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    internal class Task4
+    {
+        public static void Run()
+        {
+            Console.Write("Введіть бал: ");
+            int score = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Введіть кількість спроб: ");
+            int attempts = int.Parse(Console.ReadLine()!);
+
+            string result;
+
+            if (score >= 90)
+            {
+                result = "відмінно";
+            }
+            else if (score >= 75 && score <= 89 && attempts == 1)
+            {
+                result = "добре";
+            }
+            else if (score >= 60 && score <= 74)
+            {
+                result = "задовільно";
+            }
+            else
+            {
+                result = "незадовільно";
+            }
+
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
