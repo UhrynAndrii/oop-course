90032173e58e20fefab5f9e6328caa37b3e0c821
Андрій Угрин <uhryn.andrii@chnu.edu.ua>
2026-09-23 02:42:28 +0300

    Lab01 Task06

---
+++ Lab01/Task6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    internal class Task6
+    {
+        public static void Run()
+        {
+            Console.Write("Введіть номер студента: ");
+            int number = int.Parse(Console.ReadLine()!);
+
+            string group = (number % 10) switch
+            {
+                0 or 1 => "Група 1",
+                2 or 3 => "Група 2",
+                4 or 5 => "Група 3",
+                6 or 7 => "Група 4",
+                8 or 9 => "Група 5"
+            };
+
+            string educationForm = number % 2 == 0 ? "денна" : "вечірня";
+
+            string scholarship = number % 3 == 0 ? "так" : "ні";
+
+            Console.WriteLine($"Група: {group}");
+            Console.WriteLine($"Форма навчання: {educationForm}");
+            Console.WriteLine($"Стипендіат: {scholarship}");
+        }
+    }
+}
