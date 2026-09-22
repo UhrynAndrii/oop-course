ba92a839b9f896e45e08a3a4b06d83bea937d663
Андрій Угрин <uhryn.andrii@chnu.edu.ua>
2026-09-23 01:44:04 +0300

    Lab01 Task03

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -5,4 +5,4 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 System.Threading.Thread.CurrentThread.CurrentCulture =
     System.Globalization.CultureInfo.InvariantCulture;
 
-Task2.Run();
+Task3.Run();
---
+++ Lab01/Task3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    internal class Task3
+    {
+        public static void Run()
+        {
+            Console.Write("Введіть кількість кредитів: ");
+            int credits = int.Parse(Console.ReadLine()!);
+
+            string level;
+
+            if (credits <= 3)
+            {
+                level = "базовий";
+            }
+            else if (credits <= 6)
+            {
+                level = "середній";
+            }
+            else
+            {
+                level = "поглиблений";
+            }
+
+            Console.WriteLine($"Рівень курсу: {level}");
+        }
+    }
+}
